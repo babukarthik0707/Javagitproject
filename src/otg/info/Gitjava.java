d9cbf2977d54d65b1abad64d76cbdb7d47e398f5
babukarthik0707 <babukarthik0707@gmail.com>
2023-01-10 13:53:08 +0530

    Gitjava

---
+++ src/otg/info/Gitjava.java
@@ -0,0 +1,17 @@
+package otg.info;
+
+public class Gitjava {
+	static boolean v1;
+	static String v2;
+
+	public static void main(String[] args) {
+		System.out.println(v1);
+		System.out.println(v2);
+		String s = "INDIA";
+		int l = s.length();
+		for (int i = 0; i <= l; i++) {
+			System.out.println(s.substring(0, i));
+		}
+
+	}
+}
